732615d56f00fe879253f5269c914db90c4eb6a7
CaioSeniuk <caio.seniuk@gmail.com>
2024-06-12 08:39:39 -0300

    deletando usuários

--- aula1_SQLite.py
+++ aula1_SQLite.py
@@ -1,29 +1,35 @@
 import sqlite3
 
 # Criando banco de dados
-banco = sqlite3.connect('banco_de_dados.db')
+#banco = sqlite3.connect('banco_de_dados2.db')
 
 # Criando objeto para receber o banco
-cursor = banco.cursor() # comando sql para criar tabela, excluir, inserir registros e outras operações dentro do banco
+#cursor = banco.cursor() # comando sql para criar tabela, excluir, inserir registros e outras operações dentro do banco
 
 # Criando tabela, onde sera organizado os dados e salvar, SOMENTE uma vez
-#cursor.execute("CREATE TABLE pessoas (nome text,idade integer, email text)")
+#cursor.execute("CREATE TABLE tabela_nova (nome text,idade integer)")
 
 # Inserir dados, SOMENTE UMA VEZ
-#cursor.execute("INSERT INTO pessoas VALUES('Maria', 8, 'maria_123@gmail.com')")
+#cursor.execute("INSERT INTO tabela_nova VALUES('Maria', 20)")
 
 # Verificar dados inseridos, ver o que está no banco
-'''cursor.execute("SELECT * FROM pessoas") # Selecionar todos os dados da tabela pessoas
-print(cursor.fetchall())'''
+#cursor.execute("SELECT * FROM pessoas") # Selecionar todos os dados da tabela pessoas
+#print(cursor.fetchall())
 
-# Inserindo novo registro no banco
+# Deletar um parâmetro
+#cursor.execute("DELETE from tabela_nova WHERE idade = 20")
 
-'''cursor.execute("INSERT INTO pessoas VALUES('joao', 10, 'joao@gmail.com')")
-cursor.execute("SELECT * FROM pessoas")
-print(cursor.fetchall())'''
+# Enviar os dados pro banco
+#banco.commit()
 
-cursor.execute("DELETE FROM pessoas WHERE nome = 'joao'")
-cursor.execute("SELECT * FROM pessoas")
-print(cursor.fetchall())
-'''# Confirmando se o banco está funcionando
-banco.commit()'''
+try:
+    banco = sqlite3.connect('banco_de_dados2.db')
+    cursor = banco.cursor()
+    deletar = "joao"
+    cursor.execute(f"DELETE from tabela_nova WHERE nome = '{deletar}' ")
+    banco.commit()
+    banco.close()
+    print("Os dados foram excluídos com sucesso!")
+
+except sqlite3.Error as erro:
+    print("Erro ao excluir os dados:", erro)
